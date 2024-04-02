217ba23947e28aed89ebd6b8815bc32fa8dadff5
Cooper Spinelli <cooperspinelli@gmail.com>
2024-04-02 17:36:58 -0600

    Make changes from first code review

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -4,14 +4,14 @@ import Card from "./Card";
 
 
 /** Carousel: displays images and arrows to navigate through them
- * 
+ *
  * Props:
  * - photos: array of {src, caption} objects
  * - title: string describing the collection of images
- * 
+ *
  * State:
  * - currCardIdx: integer for current card index
- * 
+ *
  * App --> Carousel --> Card
  */
  function Carousel({ photos, title }) {
@@ -25,13 +25,19 @@ import Card from "./Card";
     setCurrCardIdx(currCardIdx + 1);
   }
 
+  //Decrements currCardIdx state by 1
+  function goBackward() {
+    setCurrCardIdx(currCardIdx - 1)
+  }
+
   return (
     <div className="Carousel">
       <h1>{title}</h1>
       <div className="Carousel-main">
         <i
           className="bi bi-arrow-left-circle"
-          onClick={goForward}
+          style={{visibility: (currCardIdx === 0 ? "hidden" : "visible") }}
+          onClick={goBackward}
         />
         <Card
           caption={currCard.caption}
@@ -41,6 +47,7 @@ import Card from "./Card";
         />
         <i
           className="bi bi-arrow-right-circle"
+          style={{visibility: (currCardIdx === photos.length - 1 ? "hidden" : "visible") }}
           onClick={goForward}
         />
       </div>
--- src/Carousel.test.jsx
+++ src/Carousel.test.jsx
@@ -51,3 +51,99 @@ it("works when you click on the right arrow", function () {
     container.querySelector('img[alt="testing image 2"]')
   ).toBeInTheDocument();
 });
+
+
+it("works when you click on the left arrow", function () {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  // expect the third image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 3"]')
+  ).toBeInTheDocument();
+
+  // move backwards in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  // expect the second image to show, but not the third
+  expect(
+    container.querySelector('img[alt="testing image 3"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+});
+
+
+it("does not display left arrow when viewing first image", function () {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // expect the right arrow to show, but not the left
+  expect(
+    container.querySelector('.bi-arrow-left-circle')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('.bi-arrow-right-circle')
+  ).toBeInTheDocument();
+});
+
+it("does not display right arrow when viewing last image", function () {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  // expect the right arrow to show, but not the left
+  expect(
+    container.querySelector('.bi-arrow-right-circle')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('.bi-arrow-left-circle')
+  ).toBeInTheDocument();
+});
+
+it("does displays both arrows when viewing second image", function () {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  // expect both right and left arrows to show
+  expect(
+    container.querySelector('.bi-arrow-right-circle')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('.bi-arrow-left-circle')
+  ).toBeInTheDocument();
+});
